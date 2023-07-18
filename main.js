d36df7ea16640dcbef29e04b7a09b320f017c926
josueventura07 <josueandresventura@gmail.com>
2023-07-18 15:49:49 -0400

    feat: form bug solved

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+/*---------------Progress Bar Animation -------------*/
+const progressBar = document.querySelectorAll('.progress_bar')
+
+document.addEventListener('scroll', e => {
+    if(scrollY >= 850) {
+        progressBar.forEach(element => {
+            element.classList.add('moveBarAnimation')
+                
+        })
+    } else {
+        progressBar.forEach(element => {
+            element.classList.remove('moveBarAnimation')
+        })
+    }    
+})
+
 /*--------------------Menu-------------------*/
-((d)=>{
+const Menu = ((d)=>{
     const $btnMenu = d.querySelector('.menu-btn'),
     $menu = d.querySelector('.menu');
 
@@ -21,15 +37,15 @@
 
 /*--------------------Contact Form-------------------*/
 
-((d)=> {
-    const $form = d.querySelector('.form-container'),
-       $loader = d.querySelector('.contact-form-loader'),
-       $response = d.querySelector('.content-response');
+const contactForm = ((d)=> {
+    const $form = d.querySelector('.contact_form'),
+       $loader = d.querySelector('.loader'),
+       $response = d.querySelector('.contact_form-response');
    
        $form.addEventListener('submit', e => {
            e.preventDefault()
            $loader.classList.remove('none');
-           fetch('https://formsubmit.co/ajax/josueandresventura@gmail.com', {
+           fetch('https://formsubmit.co/ajax/josueventura.job@gmail.com', {
                method: "POST",
                body: new FormData(e.target)
            })
@@ -52,3 +68,5 @@
                })
        })
    })(document)
+
+
